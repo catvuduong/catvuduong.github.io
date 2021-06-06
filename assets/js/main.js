8d0324ce0e3807f14fcda239899d1d62d9441df1
Cát Vũ Dương <duongcatvu17@gmail.com>
2021-06-06 23:38:54 +0700

    update audio and button.

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+let toggle = true;
+toggleAudio = () => {
+    toggle = toggle ? false : true;
+    let audio = document.getElementById('myAudio');
+    if (toggle) {
+        audio.play();
+        document.getElementById('audioButton').innerHTML = `<i class="fa fa-pause"></i>`;
+    } else {
+        audio.pause();
+        document.getElementById('audioButton').innerHTML = `<i class="fa fa-play"></i>`;
+    }
+}
+
